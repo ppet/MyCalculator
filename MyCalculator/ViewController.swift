74a48850816aed2ea2e2d06ae276c926644e37e8
Philippe petit <ppet@mac-server.home>
2015-12-28 13:54:43 +0100

    Initial Commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Philippe petit on 28/12/2015.
+//  Copyright © 2015 Philippe petit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
