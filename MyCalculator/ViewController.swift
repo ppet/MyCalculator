188b66b4a328f7cace23db4da4c5e7e1bd0e54df
Philippe petit <ppet@mac-server.local>
2015-12-28 17:11:40 +0100

    Premier commit

--- MyCalculator/ViewController.swift
+++ MyCalculator/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARKS: Properties
+    @IBOutlet weak var display: UILabel!
+    
+    // MARKS: Actions
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if display.text! == "0" {
+            display.text = digit
+        }else{
+            display.text! = display.text! + digit
+        }
+        
+    }
+    
+    // MARKS: Auto
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
